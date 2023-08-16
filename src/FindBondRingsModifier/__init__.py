ede0e08bfbe2e2478d1ff79d4d785cacb2593e3a
Constanze Kalcher <c.kalcher@googlemail.com>
2023-08-16 23:48:03 +0200

    Restructured data table and mesh generation

--- src/FindBondRingsModifier/__init__.py
+++ src/FindBondRingsModifier/__init__.py
@@ -8,11 +8,12 @@ from ovito.traits import OvitoObjectTrait
 import networkx as nx
 import numpy as np
 from ovito.vis import SurfaceMeshVis
+import math
 
 class FindBondRingsModifier(ModifierInterface):
 
     max_ring_size = Range(3, 20, label="Search for ring sizes up to")
-    min_ring_size = Range(3, 20, label="Don't show rings smaller than")
+    min_ring_size = Range(3, 20, label="Do not show rings smaller than")
 
     create_mesh = Bool(True, label="Create color meshes")
     mesh_vis = OvitoObjectTrait(SurfaceMeshVis)
@@ -22,20 +23,14 @@ class FindBondRingsModifier(ModifierInterface):
         Rings = {}
         SortedRings = {}
 
-        if self.min_ring_size < 3:
-            raise RuntimeError("Rings must consists of 3 or more bonds/edges.")
-        
-        if self.max_ring_size > 20:
-            raise RuntimeError("Rings must consists of less than 20 bonds/edges.")
-       
         if self.min_ring_size > self.max_ring_size:
-            raise RuntimeError("Max ring size must be larger than min ring size.")
+            raise RuntimeError(f"Parameter [Search for ring sizes up to] cannot be smaller than [Do not show rings smaller than.]")
 
         if not data.particles.bonds:
             raise RuntimeError("No bonds present in dataset. Please generate or import bond topology.")
         
         if data.particles.bonds.count == 0:
-            raise RuntimeError("No bonds present in dataset. Please generate or import bond topology.")
+            return
 
         for i in range(self.min_ring_size , self.max_ring_size+1):
             data.particles_.bonds_.create_property(f"N{i} Ring", data = np.zeros(data.particles.bonds.count))
@@ -53,11 +48,11 @@ class FindBondRingsModifier(ModifierInterface):
             yield i/data.particles.count
             # list of nodes connected to particle/node i with specified depth_limit
             # T is depth first search tree
-            if [bond_index for bond_index in bonds_enum.bonds_of_particle(i)] == []:
+            if len([bond_index for bond_index in bonds_enum.bonds_of_particle(i)]) == 0:
                 continue
-        
-            T = nx.dfs_tree(G, source=i, depth_limit=self.max_ring_size//2) 
-            # Find simple cycles in directed search tree (connections between particle i and max. its 3rd next neighbors)
+            T = nx.dfs_tree(G, source=i, depth_limit=math.ceil(self.max_ring_size/2))
+
+            # Find simple cycles in directed search tree (connections between particle i and max. its Nth next neighbors, where N = ceil(max_ring_size/2))
             result = nx.simple_cycles( nx.subgraph(G, T.nodes).to_directed(True))
         
             for r in result:
@@ -80,14 +75,8 @@ class FindBondRingsModifier(ModifierInterface):
                         bond_index = k if k.size > 0 else j
                         data.particles_.bonds_[f"N{r_len} Ring_"][bond_index] = 1
 
-        yield "Creating Data Tables and Color Mesh"
+        yield "Creating Data Tables"
 
-        if self.create_mesh == True:
-            mesh = data.surfaces.create(identifier='ring_mesh', title='Rings color mesh', domain=data.cell)
-            mesh.create_vertices(np.array(data.particles.positions))
-            triangle_list = []
-            face_property = []
-        
         for i in range(self.min_ring_size, self.max_ring_size+1):
             nRing_count = len(Rings[i])
             print(f"Number of {i}-membered Rings: {nRing_count} ")
@@ -95,19 +84,31 @@ class FindBondRingsModifier(ModifierInterface):
             if nRing_count  == 0:
                 continue
             data.attributes[f"N{i}-Ring count"] = nRing_count
-            table = DataTable(title=f'N{i}-Rings', plot_mode=DataTable.PlotMode.NoPlot)
+            table = data.tables.create(identifier=f'n{i}rings-table', title=f'N{i}-Rings', plot_mode=DataTable.PlotMode.NoPlot)
             table.x = table.create_property('Ring members', data=Rings[i])
             table.y = table.create_property('Ring Size', data=[len(x) for x in table.x])
-            data.objects.append(table)
+   
+        #Histogram showing ring statistics
+        table = data.tables.create(identifier='histogram', plot_mode=DataTable.PlotMode.Histogram, title='Ring Statistics')
+        table.x = table.create_property('Ring Size', data=np.arange(self.min_ring_size, self.max_ring_size+1))  
+        table.y = table.create_property('Counts', data=[len(Rings[i]) for i in range(self.min_ring_size, self.max_ring_size+1)])
 
-            if self.create_mesh == True:
+        yield "Creating Color Mesh"
+
+        if self.create_mesh == True:
+            mesh = data.surfaces.create(identifier='ring_mesh', title='Rings color mesh', domain=data.cell)
+            mesh.create_vertices(np.array(data.particles.positions))
+            triangle_list = []
+            face_property = []
+            for i in range(self.min_ring_size, self.max_ring_size+1):
+                nRing_count = len(Rings[i])
+                if nRing_count  == 0:
+                    continue
                 for ring in Rings[i]:     
                     for j in range(0,len(ring)-2):
                         vertices_of_face =  [ring[0], ring[1+j], ring[2+j]]
                         triangle_list.append(vertices_of_face)              
-                        face_property.append(i)
-        
-        if self.create_mesh == True:
+                        face_property.append(i) 
             mesh.create_faces(np.reshape(triangle_list, (-1, 3)))
             mesh.faces_.create_property("Ring Size", data = face_property)
             mesh.connect_opposite_halfedges()
